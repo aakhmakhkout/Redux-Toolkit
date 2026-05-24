48ae26ff6fb4f5f1b7f5965b2d4bdcc7fb8cbfb0
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-05-24 11:13:51 +0530

    rtk project-up: created api keys from unsplash, pexels, and giphy and used axios to create fetchData async functions

--- rtk-project/src/App.jsx
+++ rtk-project/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { fetchPhotos } from './api/mediaApi'
+import { fetchVideos } from './api/mediaApi'
+import { fetchGifs } from './api/mediaApi'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <button onClick={async ()=> {
+        const photos = await fetchPhotos("cat")
+        console.log(photos)
+      }}>Get Photos</button>
+      <button onClick={async ()=> {
+        const videos = await fetchVideos("car")
+        console.log(videos)
+      }}>
+        Get Videos
+      </button>
+      <button onClick={async ()=> {
+        const gifs = await fetchGifs("car")
+        console.log(gifs)
+      }}>
+        Get Gifs
+      </button>
+    </div>
   )
 }
 
